0810372a85f0853b9f8c1fc046564a329ecc5ab5
紺谷 怜央 <kontanireo@reokt.local>
2014-09-02 00:11:30 +0900

    modify db.php for encoding

--- config/db.php
+++ config/db.php
@@ -1,10 +1,11 @@
 <?php
   //dbの設定を記入
   $db = "digitalgrid_hems";	
-  $link = mysql_connect('127.0.0.1', 'root', 'kontani99'); 
+  $link = mysql_connect('127.0.0.1', 'root', 'password'); 
   if (!$link) {
 	die('接続失敗です。'.mysql_error());
   }
+  mysql_set_charset('utf8');
   $sdb = mysql_select_db($db,$link) or die("データベースの選択に失敗しました。");
 
 ?>
